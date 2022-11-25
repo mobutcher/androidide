7ca461e1e70267823722d4c0ef1bef885e4ccc00
mobutcher <moba0895@gmail.com>
2022-11-25 12:35:17 +0800

    任务管理器（已完成)

--- tasks/app/src/main/kotlin/com/example/tasks/MainActivity.kt
+++ tasks/app/src/main/kotlin/com/example/tasks/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -17,14 +19,47 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.example.tasks.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent { AppTheme { JetpackCompose() } }
+    setContent { 
+        AppTheme { 
+            
+            val completed = stringResource(R.string.completed)
+            
+            val work = stringResource(R.string.work)
+            
+            tasks(completed,work) 
+        } 
+    }
   }
 }
 
 @Preview
 @Composable
+fun tasks(completed: String, work: String){
+      
+      Column(
+          modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+          verticalArrangement = Arrangement.Center,
+          horizontalAlignment = Alignment.CenterHorizontally){
+          
+          Image(
+              painter = painterResource(R.drawable.ic_task_completed),        
+              contentDescription = null)
+      
+      Text(
+          text = completed,
+          fontSize = 24.sp,
+          modifier = Modifier
+          .padding(top = 24.dp,bottom = 8.dp))
+      
+      Text(text = work,
+          fontSize = 16.sp)
+      }
+    
+}
